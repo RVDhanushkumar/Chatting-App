40098bb83f63a7d6097e22dc421acb1d490f22b6
RVDhanushkumar <rapetidhanushkumar13@gmail.com>
2024-12-17 19:26:46 +0530

    backend edit

--- Chatting-web/backend/index.js
+++ Chatting-web/backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors:{
-        origin : "https://chatting-app-rouge.vercel.app",
+        origin : "*",
         methods: ["GET","POST"],
     }
 });
